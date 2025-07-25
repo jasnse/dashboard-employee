f01680598bcd2f7f377acf6945b0344677b76df5
jasnse <jasonoke03@gmail.com>
2025-07-25 13:00:18 +0700

    aaaaa

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -39,7 +39,7 @@ export default function Login() {
         <input
           type="email"
           placeholder="Email"
-          className="w-full border border-gray-300 rounded px-3 py-2 mb-3 focus:outline-none focus:ring-2 focus:ring-[#56B4B3]"
+          className="w-full border border-gray-300 rounded px-3 py-2 mb-3 focus:outline-none focus:ring-2 focus:ring-[#56B4B3] text-black"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
